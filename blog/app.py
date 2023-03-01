d5a1b89ae161b61669f442fc16fe82ac9c1aa0e6
Egor <egor.urvancev123@gmail.com>
2023-03-01 17:13:58 +0500

    lesson_1

--- blog/app.py
+++ blog/app.py
@@ -13,23 +13,3 @@ def read_user():
     name = request.args.get("name")
     surname = request.args.get("surname")
     return f"Самый лучший ученик >>> {name or '[no name]'} {surname or '[no surname]'}"
-
-@app.route("/status/", methods=["GET", "POST"])
-def custom_status_code():
-    if request.method == "GET":
-        return """\
-        To get response with custom status code
-        send request using POST method
-        and pass `code` in JSON body / FormData
-        """
-
-    print("raw bytes data:", request.data)
-
-    if request.form and "code" in request.form:
-        return "code from form", request.form["code"]
-
-    if request.json and "code" in request.json:
-        return "code from json", request.json["code"]
-    return "", 204
-
-
