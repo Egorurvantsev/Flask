23873195ff77265ae478bffc830f7aa6ce5f9939
Egor <egor.urvancev123@gmail.com>
2023-03-04 14:56:52 +0500

    fix code

--- blog/app.py
+++ blog/app.py
@@ -3,13 +3,3 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     return "Hello web!"
-
-@app.route("/greet/<name>/")
-def greet_name(name: str):
-    return f"Hello {name}!"
-
-@app.route("/user/")
-def read_user():
-    name = request.args.get("name")
-    surname = request.args.get("surname")
-    return f"Самый лучший ученик >>> {name or '[no name]'} {surname or '[no surname]'}"
