1328401e831915530c23e9404835ee88c7c3abb0
Egor <egor.urvancev123@gmail.com>
2023-04-13 16:37:22 +0500

    fixed code

--- blog/app.py
+++ blog/app.py
@@ -1,40 +1,41 @@
-from flask import Flask, redirect, url_for
-from flask_login import LoginManager
-from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
 
-from blog.auth.views import auth
-from blog.articles.views import article
-from blog.users.views import user
-
-
-db = SQLAlchemy()
-
-login_manager = LoginManager()
-
+from blog import commands
+from blog.extensions import db, login_manager, migrate
+from blog.models import User
 
 
 def create_app() -> Flask:
-    from .models import User
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = '!)zjpg+^i5wf^j6y@9j*c+vhl^_0_lk1a4pg=pqoftvl%8+!14'
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///db.sqlite"
+    app.config.from_object('blog.config')
+
+    register_extensions(app)
+    register_blueprints(app)
+    register_commands(app)
+    return app
 
 
+def register_extensions(app):
     db.init_app(app)
+    migrate.init_app(app, db, compare_type=True)
 
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
     @login_manager.user_loader
     def load_user(user_id):
-        User.query.get(int(user_id))
-
-    register_blueprints(app)
-    return app
+        return User.query.get(int(user_id))
 
 
 def register_blueprints(app: Flask):
+    from blog.auth.views import auth
+    from blog.users.views import user
+    from blog.articles.views import article
+
     app.register_blueprint(user)
-    app.register_blueprint(article)
     app.register_blueprint(auth)
+    app.register_blueprint(article)
+
+
+def register_commands(app: Flask):
+    app.cli.add_command(commands.create_init_user)
